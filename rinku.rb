5ef052bd2d4570e39004f207afe8fa5888f0c70f
xenodium <8107219+xenodium@users.noreply.github.com>
2025-11-26 13:03:39 +0000

    Adding rinku Homebrew recipe

---
+++ rinku.rb
@@ -0,0 +1,22 @@
+# Homebrew formula for rinku
+# Author: Alvaro Ramirez https://xenodium.com
+
+class Rinku < Formula
+  desc "A macOS command-line utility for fetching link previews and metadata from URLs"
+  homepage "https://github.com/xenodium/rinku"
+  url "https://github.com/xenodium/rinku/archive/refs/heads/main.tar.gz"
+  version "1.0.5"
+  license "GPL"
+
+  depends_on xcode: ["14.0", :build]
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/rinku"
+  end
+
+  test do
+    output = shell_output("#{bin}/rinku", 1)
+    assert_match "Usage:", output
+  end
+end
